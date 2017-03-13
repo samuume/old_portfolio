c6dc6b2f5315f7b503ca7ecb4e59f883203bcb67
samuume <zong0004@edu.eal.dk>
2017-03-13 18:32:40 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+gulp.task('build-css', function() {
+  return gulp.src('source/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(gulp.dest('public/assets/stylesheets'));
+});
+
+gulp.task('serve', ['build-css'], function() {
+  browserSync.init({
+    proxy: "localhost/myportfolio"
+  });
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
+  gulp.watch('public/*.html').on('change', browserSync.reload);
+});
+gulp.task('watch', ['serve']);
